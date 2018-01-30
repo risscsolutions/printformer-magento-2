ed04d6a2733b69775e44c89f68eec1448d0439bc
Andre Kalisch <aka@rissc.com>
2018-01-30 13:32:54 +0100

    Removed ajax addtocart when printformer is active

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -4,5 +4,12 @@ var config = {
             'printformer': "Rissc_Printformer/js/printformer",
             'custom-printformer': "Rissc_Printformer/js/custom-printformer.min"
         }
+    },
+    config: {
+        mixins: {
+            'Magento_Catalog/js/catalog-add-to-cart': {
+                'Rissc_Printformer/js/mixins/addtocart': true
+            }
+        }
     }
 };
