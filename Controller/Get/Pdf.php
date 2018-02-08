53d9d219b13c38ffedb8dfffef03601493a8880b
Andre Kalisch <aka@Andres-MacBook-Pro.local>
2018-02-08 07:57:52 +0100

    Fixed get/pdf controller for latest helpers

--- Controller/Get/Pdf.php
+++ Controller/Get/Pdf.php
@@ -4,52 +4,57 @@ namespace Rissc\Printformer\Controller\Get;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Rissc\Printformer\Gateway\User\Draft;
-use Rissc\Printformer\Helper\Url;
+use Rissc\Printformer\Helper\Api;
 use Rissc\Printformer\Model\ResourceModel\Draft\CollectionFactory as DraftCollectionFactory;
+use Rissc\Printformer\Model\Draft as DraftModel;
 
 class Pdf extends Action
 {
+    /** @var Api */
+    protected $_apiHelper;
 
-    protected $_draftHelper;
-
-    protected $_urlHelper;
-
+    /** @var DraftCollectionFactory */
     protected $_draftCollectionFactory;
 
+    /**
+     * Pdf constructor.
+     *
+     * @param Context                $context
+     * @param DraftCollectionFactory $draftCollectionFactory
+     * @param Api                    $apiHelper
+     */
     public function __construct(
         Context $context,
         DraftCollectionFactory $draftCollectionFactory,
-        Draft $draft,
-        Url $url
+        Api $apiHelper
     )
     {
-        $this->_draftHelper = $draft;
-        $this->_urlHelper = $url;
+        $this->_apiHelper = $apiHelper;
         $this->_draftCollectionFactory = $draftCollectionFactory;
         parent::__construct($context);
     }
 
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     * @throws \Exception
+     */
     public function execute()
     {
         $draftId = $this->getRequest()->getParam('draft_id');
         $quoteId = $this->getRequest()->getParam('quote_id');
-        $isAsync = false;
-        $draftCollection = $this->_draftCollectionFactory->create();
-        $draftItem = $draftCollection->addFieldToFilter('draft_id', ['eq' => $draftId]);
-        if ($draftItem->count() == 1) {
-            $draft = $draftItem->getFirstItem();
-            if ($draft->getProcessingId() != null) {
-                $isAsync = true;
-                $quoteId = $draft->getProcessingId();
-            }
-            //API V1 url
-            $url = $this->_urlHelper->getAdminPdfUrl($draftId, $quoteId, $isAsync);
-            //check if the draft was made with API V2
-            if ($draft->getUserIdentifier() != null) {
-                $url = $this->_draftHelper->getPdfDocument($draftId);
-            }
+
+        /** @var DraftModel $draft */
+        $draft = $this->_apiHelper->draftProcess($draftId);
+
+        if ($draftId == $draft->getDraftid()) {
+            $url = $this->_apiHelper->apiUrl()->getAdminPdf($draft->getDraftId(), $quoteId);
             $this->_redirect($url);
+
+            return;
         }
+
+        $this->_redirect($this->_url->getUrl('/'));
+
+
     }
 }
