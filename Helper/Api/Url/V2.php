0b85e2f283530b03c451802f2a144b923f699575
has <has@rissc.com>
2019-03-12 16:41:19 +0100

    Updated Review Callback Urls, Fixed bugs with review edit url and jwt auth

--- Helper/Api/Url/V2.php
+++ Helper/Api/Url/V2.php
@@ -432,7 +432,7 @@ class V2 extends AbstractHelper implements VersionInterface
 
         $calbackUrls = [
             'redirect-url' => base64_encode($this->_storeManager->getStore()->getBaseUrl()),
-            'submit-callback-url' => base64_encode($this->_storeManager->getStore()->getBaseUrl() . $callbackUrl)
+            'submit-callback-url' => base64_encode($this->_storeManager->getStore()->getBaseUrl() . '/' . $callbackUrl)
         ];
 
         $reviewEditUrl = $this->getReviewEditUrl($reviewId, $versionId) . '?' . http_build_query($calbackUrls);
