451638dc48ccc2955abfd73fb4ed190eba04edd6
Robin Scherler <ros@rissc.de>
2021-11-12 14:25:36 +0100

    WEM-1434 Moved version into config and away from admin page footer

---
+++ Block/System/Config/Form/Field/ReleaseVersion.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Rissc\Printformer\Block\System\Config\Form\Field;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Module\ModuleListInterface;
+
+class ReleaseVersion extends Field
+{
+    /**
+     * @var ModuleListInterface
+     */
+    private $moduleList;
+
+    /**
+     * @param Context $context
+     * @param ModuleListInterface $moduleList
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        ModuleListInterface $moduleList,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->moduleList = $moduleList;
+    }
+
+
+    /**
+     * Show Printformer-Extension-Version in configuration
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+
+        $html = '<div style="padding-top:7px">';
+        $html .= '<div><span> ';
+        $html .= $this->getVersion();
+        $html .= '</span></div>';
+        $html .= '</div>';
+
+        return $html;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        $result = '?';
+        $module = $this->moduleList->getOne($this->getModuleName());
+        if(isset($module['setup_version'])) {
+            $result = $module['setup_version'];
+        }
+        return $result;
+    }
+}
