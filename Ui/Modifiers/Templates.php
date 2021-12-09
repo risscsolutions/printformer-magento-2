6c22a57e1b5174662cd58774ae927760b0f46db4
Robin Scherler <ros@rissc.de>
2021-12-09 17:54:57 +0100

    Revert "fix for xml pf button admin-ui issue (tested on magento update 2.4.2-p1)"
    This reverts commit 1cded9dff5567e0b68580a9076d835108b90e53b.

--- Ui/Modifiers/Templates.php
+++ Ui/Modifiers/Templates.php
@@ -270,12 +270,10 @@ class Templates implements ModifierInterface
                                 'imports' => [
                                     'productId' => '${ $.provider }:data.product.current_product_id',
                                     'storeId' => '${ $.provider }:data.product.current_store_id',
-                                    '__disableTmpl' => ['productId' => false, 'storeId' => false],
                                 ],
                                 'exports' => [
                                     'productId' => '${ $.externalProvider }:params.current_product_id',
                                     'storeId' => '${ $.externalProvider }:params.current_store_id',
-                                    '__disableTmpl' => ['productId' => false, 'storeId' => false],
                                 ]
                             ],
                         ],
@@ -375,8 +373,7 @@ class Templates implements ModifierInterface
                             'intent' => 'intent'
                         ],
                         'links' => [
-                            'insertData' => '${ $.provider }:${ $.dataProvider }',
-                            '__disableTmpl' => ['insertData' => false],
+                            'insertData' => '${ $.provider }:${ $.dataProvider }'
                         ],
                         'sortOrder' => 2,
                     ],
