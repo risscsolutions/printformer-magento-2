ff507c41d3c4ad11eced8925bca5c4354c344b39
Robin Scherler <ros@rissc.de>
2023-01-18 15:07:28 +0100

    Create class to test cron "clean-design-images" over controller call url from docker f.e. Add adjustments about error message when path is not found and a info log whenever a file was deleted

--- Cron/CleanDesignImages.php
+++ Cron/CleanDesignImages.php
@@ -6,6 +6,7 @@ use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Filesystem\DirectoryList;
 use Magento\Framework\App\Filesystem\DirectoryList as MagentoDirectoryList;
 use Magento\Framework\Filesystem\Driver\File;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Psr\Log\LoggerInterface;
 
 class CleanDesignImages
@@ -16,6 +17,9 @@ class CleanDesignImages
     const DIRECTORY = 'printformer';
     const DIRECTORY_PREVIEW = 'preview';
     const DIRECTORY_THUMBS = 'thumbs';
+    const WEEK_IN_SECONDS = 604800;
+    const DAY_IN_SECONDS = 86400;
+    private TimezoneInterface $timezone;
 
     /**
      * @param LoggerInterface $logger
@@ -25,12 +29,14 @@ class CleanDesignImages
     public function __construct(
         LoggerInterface $logger,
         DirectoryList $directoryList,
-        File $file
+        File $file,
+        TimezoneInterface $timezone
     )
     {
         $this->logger = $logger;
         $this->directoryList = $directoryList;
         $this->file = $file;
+        $this->timezone = $timezone;
     }
 
     public function execute()
@@ -43,22 +49,27 @@ class CleanDesignImages
                 . MagentoDirectoryList::MEDIA . DIRECTORY_SEPARATOR . $this::DIRECTORY . DIRECTORY_SEPARATOR
                 . $subFolder;
 
-            //read just that single directory
-            $subFolderFiles =  $this->file->readDirectory($subFolderPath);
+            if (is_dir($subFolderPath)) {
+                //read just that single directory
+                $subFolderFiles =  $this->file->readDirectory($subFolderPath);
 
-            foreach ($subFolderFiles as $subFolderFile){
-                if (file_exists($subFolderFile)){
-                    if (time()-filemtime($subFolderFile) > 168 * 3600){
-                        try {
-                            $this->file->deleteFile($subFolderFile);
-                            $this->logger->info(
-                                'This design image is older then 7 days and will be deleted now: '
-                                . $subFolderFile
-                            );
-                        } catch (FileSystemException $e) {
+                foreach ($subFolderFiles as $subFolderFile){
+                    if (file_exists($subFolderFile)){
+                        $fileModificationTime = time()-filemtime($subFolderFile);
+                        if ($fileModificationTime > $this::WEEK_IN_SECONDS){
+                            try {
+                                $this->file->deleteFile($subFolderFile);
+                                $this->logger->info(
+                                    'Deleted Cached Image: '
+                                    . $subFolderFile.  ' (after more then 7 days) (' . number_format($fileModificationTime/$this::DAY_IN_SECONDS, 1) .' days old) on: '. $this->timezone->date()->format('Y-m-d H:i:s')
+                                );
+                            } catch (FileSystemException $e) {
+                            }
                         }
                     }
                 }
+            } else {
+                $this->logger->warning('Image cleanup not possible. Can not access directory on path: '.$subFolderPath);
             }
         }
     }
