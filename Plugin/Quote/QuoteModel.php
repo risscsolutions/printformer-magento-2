1c8d1b30c5f6e130637a557f78f6c54eb1309190
Robin Scherler <ros@rissc.de>
2023-02-15 15:36:58 +0100

    fix(WEM-2048): fix issue with "Add to cart" button on category page
    The "Add to cart" button on the category page was not working correctly when adding a product to the cart from the "Recently Ordered" section. This was due to incorrect information being saved in the draft, which prevented the draft from being opened in the editor.
    
    This commit fixes the issue by updating the information in the draft and resolving the "draft cannot be opened by you" error message.
    
    Fixes #WEM-2048

--- Plugin/Quote/QuoteModel.php
+++ Plugin/Quote/QuoteModel.php
@@ -2,13 +2,15 @@
 
 namespace Rissc\Printformer\Plugin\Quote;
 
+use Closure;
+use Exception;
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Type\AbstractType;
 use Magento\Framework\DataObject;
 use Magento\Quote\Model\Quote\Item;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Quote\Model\Quote as SubjectQuote;
 use Rissc\Printformer\Helper\Session;
-use Rissc\Printformer\Model\Draft;
 use Rissc\Printformer\Setup\InstallSchema;
 use Psr\Log\LoggerInterface;
 use Rissc\Printformer\Helper\Api as ApiHelper;
@@ -36,20 +38,31 @@ class QuoteModel
     /**
      * @var ApiHelper
      */
-    protected $_apiHelper;
+    protected $apiHelper;
 
+    /**
+     * @var Registry $registry
+     */
     private Registry $registry;
 
+    /**
+     * @var CartHelper
+     */
     private CartHelper $cartHelper;
+
+    /**
+     * @var ConfigHelper
+     */
     private ConfigHelper $configHelper;
 
     /**
-     * QuoteModel constructor.
      * @param StoreManagerInterface $storeManager
      * @param Session $session
      * @param LoggerInterface $logger
      * @param ApiHelper $apiHelper
      * @param Registry $registry
+     * @param CartHelper $cartHelper
+     * @param ConfigHelper $configHelper
      */
     public function __construct(
         StoreManagerInterface $storeManager,
@@ -59,11 +72,12 @@ class QuoteModel
         Registry $registry,
         CartHelper $cartHelper,
         ConfigHelper $configHelper
-    ) {
+    )
+    {
         $this->storeManager = $storeManager;
         $this->session = $session;
         $this->logger = $logger;
-        $this->_apiHelper = $apiHelper;
+        $this->apiHelper = $apiHelper;
         $this->registry = $registry;
         $this->cartHelper = $cartHelper;
         $this->configHelper = $configHelper;
@@ -71,80 +85,27 @@ class QuoteModel
 
     /**
      * @param SubjectQuote $subject
-     * @param \Closure $proceed
+     * @param Closure $proceed
      * @param Product $product
      * @param null|float|DataObject $buyRequest
      * @param $processMode
      * @return Item|mixed|string
-     * @throws \Exception
      */
     public function aroundAddProduct(
         SubjectQuote $subject,
-        \Closure $proceed,
+        Closure $proceed,
         Product $product,
         $buyRequest = null,
-        $processMode = \Magento\Catalog\Model\Product\Type\AbstractType::PROCESS_MODE_FULL
-    ) {
-        if(!$buyRequest instanceof DataObject)
+        $processMode = AbstractType::PROCESS_MODE_FULL
+    )
+    {
+        if (!$buyRequest instanceof DataObject)
             return $proceed($product, $buyRequest, $processMode);
 
         $currentOrder = $this->registry->registry('current_order');
         (!empty($currentOrder)) ? $isReordered = true : $isReordered = false;
 
-        $draftIds = $buyRequest->getData(InstallSchema::COLUMN_NAME_DRAFTID);
-        if (!empty($draftIds)) {
-            $draftHashArray = explode(',', $draftIds);
-
-            $draftHashRelations = [];
-            $newDraftHashArray = [];
-            foreach ($draftHashArray as $draftId) {
-                if ($draftId == '') {
-                    continue;
-                }
-
-                if ($isReordered) {
-                    $oldDraftId = $draftId;
-                    $customerId = $this->session->getCustomerSession()->getCustomerId();
-                    $newDraftProcess = $this->_apiHelper->generateNewReplicateDraft($oldDraftId, $customerId);
-                    if (!empty($newDraftProcess)) {
-                        $newDraftId = $newDraftProcess->getDraftId();
-                        if (!empty($newDraftId)) {
-                            $draftId = $newDraftId;
-                            $relations = $buyRequest->getData('draft_hash_relations');
-                            if (!empty($relations[$newDraftProcess->getProductId()][$newDraftProcess->getPrintformerProductId()])) {
-                                $relations[$newDraftProcess->getProductId()][$newDraftProcess->getPrintformerProductId()] = $newDraftId;
-                                $buyRequest->setData('draft_hash_relations', $relations);
-                            }
-                        }
-                    }
-                }
-
-                if (empty($newDraftProcess)) {
-                    /** @var Draft $draftProcess */
-                    $draftProcess = $this->_apiHelper->draftProcess($draftId);
-                } else {
-                    $draftProcess = $newDraftProcess;
-                }
-
-                if ($draftProcess->getId()) {
-                    $draftHashRelations = $this->cartHelper->updateDraftHashRelations(
-                        $draftHashRelations,
-                        $draftProcess->getProductId(),
-                        $draftProcess->getPrintformerProductId(),
-                        $draftProcess->getDraftId()
-                    );
-                }
-
-                array_push($newDraftHashArray, $draftId);
-            }
-
-            $newDraftHashArrayFormatted = implode(',', $newDraftHashArray);
-            $buyRequest->setData(InstallSchema::COLUMN_NAME_DRAFTID, $newDraftHashArrayFormatted);
-
-            if (!empty($draftHashRelations)) {
-                $buyRequest->setData('draft_hash_relations', $draftHashRelations);
-            }
-        }
+        $this->cartHelper->prepareDraft($isReordered, $isReordered);
 
         $result = $proceed($product, $buyRequest, $processMode);
 
@@ -153,19 +114,20 @@ class QuoteModel
 
     /**
      * Set printformer data for updated quote item
-     *
      * @param SubjectQuote $subject
      * @param $result
      * @return Item|string
      */
-    public function afterUpdateItem(SubjectQuote $subject, $result)
+    public function afterUpdateItem(
+        SubjectQuote $subject,
+        $result
+    )
     {
         return is_string($result) ? $result : $this->setPrintformerData($result);
     }
 
     /**
      * Load printformer data to quote-item
-     *
      * @param Item $item
      * @return Item
      */
@@ -175,7 +137,7 @@ class QuoteModel
         $this->loadPrintformerDataByQuoteItem($item);
 
         //load pf-data for child-product-item
-        if($this->configHelper->useChildProduct($item->getProductType())) {
+        if ($this->configHelper->useChildProduct($item->getProductType())) {
             $childItems = $item->getChildren();
             if (!empty($childItems)) {
                 $firstChildItem = $childItems[0];
@@ -188,7 +150,6 @@ class QuoteModel
 
     /**
      * Set draft ids from buy-request to quote-item
-     *
      * @param Item\AbstractItem $quoteItem
      * @return void
      */
@@ -201,7 +162,7 @@ class QuoteModel
                     $storeId = $this->storeManager->getStore()->getId();
                     $buyRequest = $quoteItem->getBuyRequest();
                     $draftHashRelations = $buyRequest->getData('draft_hash_relations');
-                    if (isset($draftHashRelations) && is_array($draftHashRelations)){
+                    if (isset($draftHashRelations) && is_array($draftHashRelations)) {
                         if (isset($draftHashRelations[$quoteItem->getProduct()->getId()])) {
                             $newDraftHashes = $draftHashRelations[$quoteItem->getProduct()->getId()];
                             $newDraftHashField = implode(',', $newDraftHashes);
@@ -212,7 +173,7 @@ class QuoteModel
                     }
                 }
             }
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $this->logger->critical($e);
         }
     }
