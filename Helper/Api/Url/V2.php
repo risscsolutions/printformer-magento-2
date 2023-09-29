15a19307912ca6835820f5f0fd14452a80ffe65d
alexander.d.t@webmeridian.org <alexander.d.t@webmeridian.org>
2023-09-29 16:18:30 +0300

    fix(WEM-2280): update editor url store configuration for admin draft page

--- Helper/Api/Url/V2.php
+++ Helper/Api/Url/V2.php
@@ -232,7 +232,13 @@ class V2 extends AbstractHelper implements VersionInterface
         $params = []
     )
     {
-        $editorUrl = $this->getPrintformerBaseUrl() .
+        $storeId = $this->_storeManager->getStore()->getId();
+        // Check store param for admin pages
+        if (isset($params['store_id'])){
+            $storeId = $params['store_id'];
+        }
+
+        $editorUrl = $this->getPrintformerBaseUrl($storeId) .
             self::EXT_EDITOR_PATH;
 
         $dataParams = [
@@ -252,12 +258,12 @@ class V2 extends AbstractHelper implements VersionInterface
         $queryParams = [];
         $queryParams['callback'] = $this->_getCallbackUrl(
             $customCallbackUrl,
-            $this->_storeManager->getStore()->getId(),
+            $storeId,
             $dataParams
         );
 
         if ($this->_config->getRedirectProductOnCancel()) {
-            $queryParams['callback_cancel'] = $this->_getProductCallbackUrl(intval($params['product_id']), $params['data'], $this->_storeManager->getStore()->getId());
+            $queryParams['callback_cancel'] = $this->_getProductCallbackUrl(intval($params['product_id']), $params['data'], $storeId);
         }
 
         return $editorUrl . '/' . $draftHash . '?' . http_build_query($queryParams);
